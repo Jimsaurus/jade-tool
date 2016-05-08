9ff6d0e2c3cc3b94e74e8c8e78c4e2dee6527a12
James McDermid <jmcdermid@demacmedia.com>
2016-05-07 23:12:45 -0400

    Finished Work first pass

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,14 @@ app.init = function(){
 	    slidesToShow: 3,
 	    slidesToScroll: 1,
         infinite: true,
+        responsive:[
+        	{
+        		breakpoint:768,
+        		settings:{
+        			slidesToShow: 2
+        		}
+        	}
+        ]
 	});
 	// =============================================================================
 	// Mobile Menu
@@ -136,7 +144,48 @@ app.init = function(){
        return false;
      });
 
+   // =============================================================================
+   // Isotope Init
+   // =============================================================================
 
+   // init Isotope
+   var $grid = $('.grid').isotope({
+   	itemSelector: '.color-shape'
+   });
+
+   // store filter for each group
+   var filters = {};
+
+   $('.filters').on( 'click', '.button', function() {
+	   	var $this = $(this);
+	     // get group key
+	     var $buttonGroup = $this.parents('.button-group');
+	     var filterGroup = $buttonGroup.attr('data-filter-group');
+	     // set filter for group
+	     filters[ filterGroup ] = $this.attr('data-filter');
+	     // combine filters
+	     var filterValue = concatValues( filters );
+	     // set filter for Isotope
+	     $grid.isotope({ filter: filterValue });
+ 	});
+
+   // change is-checked class on buttons
+   $('.button-group').each( function( i, buttonGroup ) {
+   	var $buttonGroup = $( buttonGroup );
+   	$buttonGroup.on( 'click', 'button', function() {
+   		$buttonGroup.find('.is-checked').removeClass('is-checked');
+   		$( this ).addClass('is-checked');
+   	});
+   });
+
+   // flatten object by concatting values
+   function concatValues( obj ) {
+	   	var value = '';
+	   	for ( var prop in obj ) {
+	   		value += obj[ prop ];
+	   	}
+	   	return value;
+   }
 
 }; // end init function
 
